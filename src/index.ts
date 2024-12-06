245d5689b9088e7aec1f1982fb6a6b0e0156ac3d
Max Duval <max.duval@hometrack.com>
2024-12-06 09:32:52 +0000

    feat: initial commit
    TypeScript starter template

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/// <reference types="@fastly/js-compute" />
+import { env } from "fastly:env";
+
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+
+async function handleRequest(event: FetchEvent) {
+  // Log service version
+  console.log(
+    "FASTLY_SERVICE_VERSION:",
+    env("FASTLY_SERVICE_VERSION") || "local",
+  );
+
+  const body = JSON.stringify({ ...event.client.geo }, null, 2);
+
+  return new Response(body, {
+    status: 200,
+    headers: new Headers({ "Content-Type": "application/json; charset=utf-8" }),
+  });
+}
